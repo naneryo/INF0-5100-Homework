c76aac290ccb8aaf82acf320281ef959df760707
naneryo <yaonaner1211@gmail.com>
2026-09-21 20:32:36 -0700

    change to it is my first program

--- java_projects/first_project/src/main/java/edu/neu/mgen/HelloApp.java
+++ java_projects/first_project/src/main/java/edu/neu/mgen/HelloApp.java
@@ -8,6 +8,6 @@ public class HelloApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "It is my first Java program" ); //hello program
+        System.out.println( "It is my first Java program" ); //It is my first Java program
     }
 }
