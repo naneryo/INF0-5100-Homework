7d39175ea47eab548ba3fe572e3cc4459ebcdfeb
naneryo <yaonaner1211@gmail.com>
2026-09-21 20:25:39 -0700

    change-to-helloprogram

--- java_projects/first_project/src/main/java/edu/neu/mgen/HelloApp.java
+++ java_projects/first_project/src/main/java/edu/neu/mgen/HelloApp.java
@@ -8,6 +8,6 @@ public class HelloApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello program!" ); //hello program
     }
 }
