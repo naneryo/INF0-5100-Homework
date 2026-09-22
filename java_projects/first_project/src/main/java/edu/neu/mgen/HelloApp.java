6de4175843d54354a01c43e2e447773915b93129
naneryo <yaonaner1211@gmail.com>
2026-09-21 20:22:50 -0700

    create-java-program-1.0

---
+++ java_projects/first_project/src/main/java/edu/neu/mgen/HelloApp.java
@@ -0,0 +1,13 @@
+package edu.neu.mgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
